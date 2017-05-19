ed4c264d36ccd877fe84f306d6ba0ea4988cb228
eztam <mbirschl@gmail.com>
2017-05-19 20:26:11 +0200

    Javadoc; clean up

--- Backtesting/src/main/java/com/early_reflections/Strategy.java
+++ Backtesting/src/main/java/com/early_reflections/Strategy.java
@@ -11,10 +11,13 @@ public abstract class Strategy {
 
 	private List<Indicator> indicators = new ArrayList<>();
 
+	/**
+	 * For internal use only.
+     */
 	public final Trade processTick(Quote quote) {
 		this.quotes.add(quote);
 		for(Indicator i: indicators){
-		    i.performTradingDay(quote);
+		    i.processTick(quote);
         }
 		Trade trade = tick(quote);
 		return trade == null ? new Trade() : trade;
@@ -23,27 +26,32 @@ public abstract class Strategy {
 	/**
 	 * Is called on each quote tick (e.g. daily).
 	 * 
-	 * @param quote The quote of the current tick
-	 * @return  A buy or sell trade if the strategy decides to perform a trade. Otherwise null.
+	 * @param quote The quote of the current tick.
+	 * @return A buy or sell trade if the strategy decides to perform a trade. Otherwise null.
 	 */
 	protected abstract Trade tick(Quote quote);
 
     /**
-     *
-     * @return All historic quotes
+     * @return All historic quotes.
      */
 	public final List<Quote> getQuotes() {
 		return quotes;
 	}
 
-
+	/**
+	 * Registers a new {@link Indicator}. Indicators will automatically be calculated on the fly on each tick.
+	 * Multiple indicators can be added and all of them are drawn on the main chart.
+	 * @param indicator
+	 */
 	protected final void registerIndicator(Indicator indicator){
         indicators.add(indicator);
     }
 
+	/**
+	 * @return A list of all registered {@link Indicator}s.
+	 */
     public final List<Indicator> getIndicators(){
 	    return indicators;
     }
-
 
 }
--- Backtesting/src/main/java/com/early_reflections/indicators/Indicator.java
+++ Backtesting/src/main/java/com/early_reflections/indicators/Indicator.java
@@ -15,28 +15,29 @@ public abstract class Indicator {
     private SortedMap<LocalDate, Double> values = new ConcurrentSkipListMap<>();
 
     /**
-     * @return A unique identifier for the indicator.
+     * @return A unique identifier for the indicator. This identifier is also used as label on the chart.
      */
     public abstract String getId();
 
     /**
      * Gets called on each tick e.g. daily.
      *
-     * @param quote The current quote
+     * @param quote The current quote.
      * @return The calculated indicator value for the current tick or null if the indicator has no value for the current tick.
      */
     protected abstract Double tradingDayTick(Quote quote);
 
-    public SortedMap<LocalDate, Double> getValues() {
+    /**
+     * @return The calculated indicator values for all historic quotes.
+     */
+    public final SortedMap<LocalDate, Double> getValues() {
         return values;
     }
 
     /**
      * For internal usage only.
-     *
-     * @param quote
      */
-    public final void performTradingDay(Quote quote) {
+    public final void processTick(Quote quote) {
         this.quotes.add(quote);
         Double value = tradingDayTick(quote);
         if (value != null) {
@@ -44,12 +45,23 @@ public abstract class Indicator {
         }
     }
 
+    /**
+     * @return All historic quotes.
+     */
     public final List<Quote> getQuotes() {
         return quotes;
     }
 
     @Override
-    public int hashCode() {
+    public final int hashCode() {
         return getId().hashCode();
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        if(obj instanceof Indicator){
+            return getId().equals(((Indicator)obj).getId());
+        }
+        return false;
     }
 }
